accaa6817efa970911877dfc519aa63fc5f0e08e
Jianxiong Xiao (Professor X) <jianxiongxiao@gmail.com>
2016-09-07 22:22:21 -0700

    recursively search for png and convert it to jpg
    preserving folder structure

---
+++ png2jpg.m
@@ -0,0 +1,27 @@
+imageRootPath = 'png/';
+new_folder = 'jpg/';
+
+
+folders = genpath(imageRootPath);
+folders = regexp(folders,':','split');
+folders = folders(1:end-1);
+imageList = {};
+for f=1:length(folders)
+    imagefiles = dir(fullfile(folders{f},'*.png'));
+    if ~isempty(imagefiles)
+        
+        new_folder=fullfile(new_folder, folders{f});
+        if ~exist(new_folder,'dir')
+            mkdir(new_folder);
+        end
+        
+        for i=1:length(imagefiles)
+            imageList{end+1} = fullfile(folders{f},imagefiles(i).name);
+            
+            % png to jpg
+            im = imread(imageList{end});
+            imwrite(im, fullfile(new_folder, [imagefiles(i).name 'jpg']));
+        end
+    end
+end
+
